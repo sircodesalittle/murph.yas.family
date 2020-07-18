005c207128ed63eee70218232f9e2da58c23f6b0
Alex Dykstra <adykstra3@gatech.edu>
2020-07-18 08:28:18 -0400

    Updating next murph date

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ class IndexComponent extends React.Component {
     
     return (
       <div>
-          <h1 class="rainbow rainbow_text_animated">Next YAS Murph - 7:00 AM July 11 @ Alex's </h1>
+          <h1 class="rainbow rainbow_text_animated">Next YAS Murph - 7:00 AM August 8 @ Alex's </h1>
           <pre style={{textAlign: 'center'}}>Results:</pre>
           {murphTables.map((table, i) => (
             <pre style={{textAlign: 'center'}} key={i}>{table.toString()}</pre>
